5abce56e329c167ae3b7f893488ec9ace5781d61
Johan F. <johan.artmat@gmail.com>
2024-02-21 00:34:50 -0500

    inicio creación de estrategia

--- Colas/user.py
+++ Colas/user.py
@@ -1,21 +1,35 @@
 from pago import Pago
 
 class Usuario:
-  def __init__(self, nombre: str, correo: str, pago:  Pago) -> None:
+  def __init__(self, nombre: str, correo: str, pago: Pago) -> None:
     self._nombre: str = nombre
     self._correo: str = correo
-    self._pago: Pago = pago
+    self._pago: Pago = pago 
 
 class UsuarioPrivilegiado(Usuario):
-  def __init__(self, nombre: str, correo: str, monto:  float) -> None:
-    super(nombre, correo)
+  def __init__(self, nombre: str, correo: str, pago: Pago) -> None:
+    super().__init__(nombre, correo, pago)
     self.descuento: float = 0.4
     self.promocion: bool = True
-    self._monto: float = 3000000
+    self._pago.establecer_monto(5000000)
+    self._tipo: str = 'PRIVILEGIADO'
+
+  def pagar(self, monto: float) -> bool:
+    return self._pago.pagar(monto*self.descuento)
+  
+  def obtener_tipo(self) -> str:
+    return self._tipo+':'+self._pago.obtener_tipo()
 
 class UsuarioIntermedio(Usuario):
-  def __init__(self, nombre: str, correo: str, monto:  float) -> None:
-    super(nombre, correo)
+  def __init__(self, nombre: str, correo: str, pago: Pago) -> None:
+    super().__init__(nombre, correo, pago)
     self.descuento: float = 0.2
     self.promocion: bool = False
-    self._monto: float = 1500000
+    self._pago.establecer_monto(3000000)
+    self._tipo: str = 'INTERMEDIO'
+
+  def pagar(self, monto: float) -> bool:
+    return self._pago.pagar(monto*self.descuento)
+  
+  def obtener_tipo(self) -> str:
+    return self._tipo+':'+self._pago.obtener_tipo()

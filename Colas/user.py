eae1d0522b174f058fd3181a3d920fed44954ea1
juandaniel13 <juandanielrh13@gmail.com>
2024-02-20 23:21:57 -0500

    añadiendo pago al usuario y monto a los tipos de usuario

--- Colas/user.py
+++ Colas/user.py
@@ -1,17 +1,21 @@
+from pago import Pago
 
 class Usuario:
-  def __init__(self, nombre: str, correo: str) -> None:
+  def __init__(self, nombre: str, correo: str, pago:  Pago) -> None:
     self._nombre: str = nombre
     self._correo: str = correo
+    self._pago: Pago = pago
 
 class UsuarioPrivilegiado(Usuario):
-  def __init__(self, nombre: str, correo: str) -> None:
+  def __init__(self, nombre: str, correo: str, monto:  float) -> None:
     super(nombre, correo)
     self.descuento: float = 0.4
     self.promocion: bool = True
+    self._monto: float = 3000000
 
 class UsuarioIntermedio(Usuario):
-  def __init__(self, nombre: str, correo: str) -> None:
+  def __init__(self, nombre: str, correo: str, monto:  float) -> None:
     super(nombre, correo)
     self.descuento: float = 0.2
     self.promocion: bool = False
+    self._monto: float = 1500000
